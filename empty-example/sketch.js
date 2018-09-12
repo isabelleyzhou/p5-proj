1220400bb51c4a4bec5a18a928f4cc859bebed2b
Isabelle Zhou <isabellezhou@Isabelles-MacBook-Pro.local>
2018-09-12 16:43:54 -0700

    title and instructions started

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,8 +1,43 @@
+var canvas;
+
 function setup() {
-  // put setup code here
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-    ellipse(50, 50, 80, 80);
-  
+  background(51);
+  fill(255);
+
+  fill(255, 255, 255);
+  textSize(60)
+  text('Recursion', window.innerWidth *.3 , 80);
+
+
+
+  fill(250, 255, 255);
+  textSize(15)
+  text('We will be creating a recursive function to find factorials: ', window.innerWidth *.01 , 150);
+
+
+}
+
+window.onresize = function() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  canvas.size(w,h);
+  width = w;
+  height = h;
+};
+
+function mouseDragged(){
+  value = value + 5;
+  if (value > 255) {
+    value = 0;
+  }
+}
+
+function mouseDragged(){
+  rect(mouseX, mouseY, 50, 50);
+
+  return false;
 }
